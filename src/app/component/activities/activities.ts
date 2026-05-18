b2e7730460d26368d31116f01e915c36dc8613c6
rltsolution2025 <social.rltdigital@gmail.com>
2026-05-18 10:28:47 +0530

    Mobile View Updated

--- src/app/component/activities/activities.ts
+++ src/app/component/activities/activities.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-activities',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './activities.html',
   styleUrl: './activities.css',
 })
--- src/app/component/smart-classes/smart-classes.ts
+++ src/app/component/smart-classes/smart-classes.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-smart-classes',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './smart-classes.html',
   styleUrl: './smart-classes.css',
 })
